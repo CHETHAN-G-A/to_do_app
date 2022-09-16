72536a0cfd3daebff425e89d6e5e2047270f056d
CHETHAN-G-A <gachethan@gmail.com>
2022-09-16 19:38:16 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 from flask import Flask, render_template ,request ,redirect ,url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField,SelectField
-from wtforms.validators import DataRequired
-from flask_bootstrap import Bootstrap
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
-Bootstrap(app)
-
-
 
 #CONNECT TO DB
 
