0ed97c56830846522ece91d6b58f3364c5efc30c
CHETHAN-G-A <gachethan@gmail.com>
2022-09-16 19:38:16 +0530

    Initial commit

---
+++ forms.py
@@ -0,0 +1,27 @@
+from flask_wtf.file import FileRequired, FileAllowed ,DataRequired
+from wtforms import Form, BooleanField, StringField, PasswordField,IntegerField, validators , TextAreaField, FileField
+
+## Login Form ##
+class LoginForm(Form):
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('Password', [validators.DataRequired()])
+
+## Registration form ##
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+
+## Todoform ##
+class TodoForm(Form):
+    title = StringField('Title', [validators.Length(min=6, max=35)])
+    description = StringField('Description', [validators.DataRequired()])
+
+## Update Form ##
+class UpdateForm(Form):
+    title = StringField('Title', [validators.Length(min=6, max=35)])
+    description = StringField('Description', [validators.DataRequired()])
--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 from flask import Flask, render_template ,request ,redirect ,url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField,SelectField
-from wtforms.validators import DataRequired
-from flask_bootstrap import Bootstrap
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from forms import LoginForm, RegistrationForm, TodoForm ,UpdateForm
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-Bootstrap(app)
 
 
-
-#CONNECT TO DB
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-#users db
-
+#Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 #CONFIGURE TABLES
 class Todo(db.Model):
@@ -41,67 +31,80 @@ class User(db.Model):
     name = db.Column(db.String(1000))
 db.create_all()
 
+
+##Login page##
 @app.route("/login",methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email_add')
-        password = request.form.get('password')
-        user = User.query.filter_by(email=request.form["email_add"]).first()
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User.query.filter_by(email=form.email.data).first()
         if not user:
             flash("That email does not exist, please try again.")
             return redirect(url_for('login'))
-        elif user.password != request.form["password"]:
-            flash("Incorrect Password")
+        elif user and bcrypt.check_password_hash(user.password, form.password.data):
+            flash(f'welcome {user.email} you are logged in now')
+            user = user.id
+            return redirect(url_for('todo', id=user))
+        else:
+            flash('wrong password please try again')
             return redirect(url_for('login'))
-        if user.password==request.form["password"]:
-            x = user.id
-            return redirect(url_for('home',id=x))
+    return render_template('login.html', form=form)
 
-    return render_template("login.html")
+##Register page##
+@app.route("/register" , methods=['GET','POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(name=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
-@app.route("/<int:id>" , methods=['GET','POST'])
-def home(id):
+## Main page ##
+@app.route("/todo/<int:id>" , methods=['GET','POST'])
+def todo(id):
+    form =TodoForm(request.form)
     if request.method == 'POST':
-        new_todo = Todo(title=request.form["title"],desc=request.form["desc"],user_id=id)
+        new_todo = Todo(title=form.title.data,desc=form.description.data,user_id=id)
         db.session.add(new_todo)
         db.session.commit()
+        return redirect(url_for('todo',id=id))
     all_todo = Todo.query.filter_by(user_id=id)
+    return render_template("index.html" , all_todo=all_todo ,user=id , form=form)
 
-    return render_template("index.html" , all_todo=all_todo ,user=id)
 
-
-@app.route("/delete/<int:sno>")
-def delete(sno):
+@app.route("/delete/<int:sno>/<int:user_id>")
+def delete(sno,user_id):
     todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for("home"))
+
+    user = user_id
+    return redirect(url_for('todo',id=user))
+
 
 @app.route("/update/<int:sno>",methods=['GET','POST'])
 def update(sno):
+    form = UpdateForm(request.form)
     if request.method == 'POST':
-        title = request.form["title"]
-        desc = request.form["desc"]
+        title = form.title.data
+        desc = form.description.data
         todo = Todo.query.filter_by(sno=sno).first()
         todo.title = title
         todo.desc = desc
         db.session.add(todo)
         db.session.commit()
         user = todo.user_id
-        return redirect(url_for('home',id=user))
+        return redirect(url_for('todo',id=user))
     todo = Todo.query.filter_by(sno=sno).first()
-    return render_template("update.html",todo=todo)
+    return render_template("update.html",todo=todo ,form=form)
 
 
 
-@app.route("/register" , methods=['GET','POST'])
-def register():
-    if request.method == 'POST':
-        new_user = User(email=request.form["email_add"], password=request.form["password"],name=request.form["name"])
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template("register.html")
+
 
 if __name__=='__main__':
     app.run(debug=True)
